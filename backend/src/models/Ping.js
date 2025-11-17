f4382d9d58ca1be8f3563a82ab5b935ee1cfae1c
chocoham <banma1234@gmail.com>
2025-11-17 17:20:54 +0900

    chore: 불필요 파일 제거, 파일명 변경

--- backend/src/models/Ping.js
+++
@@ -1,8 +0,0 @@
-const { Schema, model } = require('mongoose');
-
-const PingSchema = new Schema({
-  at: { type: Date, default: Date.now },
-  note: { type: String, default: '' },
-}, { versionKey: false });
-
-module.exports = model('Ping', PingSchema);
